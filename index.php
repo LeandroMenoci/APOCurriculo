ad64c62beca0b1a41af6ba2ee4dffe92caa5638f
Leandro <menoci.leandro@gmail.com>
2025-06-01 12:42:17 -0700

    index

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="container mx-auto p-6">
+  <h1 class="text-3xl font-bold mb-4">Bem-vindo ao Gerador de Currículos</h1>
+  <p class="mb-4">Crie seu currículo de forma simples e rápida!</p>
+  <a href="views/form.php" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">Criar Currículo</a>
+</div>
+
+<?php include 'includes/footer.php'; ?>
